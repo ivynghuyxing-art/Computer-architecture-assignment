df54502c38d0431197b964336e51012f40f5a1b5
ivy <ivynghuyxingivy@gmail.com>
2026-08-29 22:16:56 +0800

    add

--- login/login.asm
+++ login/login.asm
@@ -42,6 +42,7 @@ msg21   db 	10,13,"Login Successfully!$"
 msg22   db 	10,13,"Invalid username or password!$"
 msg23   db 	10,13,"Register Successfully!$"
 msg24   db 	10,13,"Invalid email ! Please enter correct email !"
+
 reg_username    db 	21 dup(0)
 reg_password    db 	21 dup(0)
 
